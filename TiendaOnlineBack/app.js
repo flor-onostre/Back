5a295f5c6423f9c82f147f7041b53158fab0a646
Florencia Onostre <flonostre3010@gmail.com>
2025-01-05 23:41:44 -0300

    actualización

--- TiendaOnlineBack/app.js
+++ TiendaOnlineBack/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { router as authRoutes } from './routes/authRoutes.js';
-import productRoutes from './routes/productRoutes.js';
+import { router as authRoutes } from './src/routes/authRoutes.js';
+import productRoutes from './src/routes/productRoutes.js';
 
 dotenv.config();
 
